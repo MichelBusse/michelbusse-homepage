c8310f007928800834c18d4b03d65c441916bb52
Michel Busse <michel.busse@m-to-b.com>
2024-02-23 00:19:41 +0100

    Added links to images in portfolio section

--- app/components/Portfolio.tsx
+++ app/components/Portfolio.tsx
@@ -13,12 +13,14 @@ const Portfolio = () => {
       <section className={styles.portfolio}>
         <div className={styles.project}>
           <div className={styles.imageWrapper}>
-            <Image
-              src={"/images/portfolio/clubz-social-network-app.png"}
-              alt={"Social Network App - Clubz"}
-              width="800"
-              height="800"
-            />
+            <Link href={"/projects/social-network-app"}>
+              <Image
+                src={"/images/portfolio/clubz-social-network-app.png"}
+                alt={"Social Network App - Clubz"}
+                width="800"
+                height="800"
+              />
+            </Link>
             <div className={styles.textWrapper}>
               <h3>Social Network App</h3>
               <p>
@@ -42,12 +44,14 @@ const Portfolio = () => {
         </div>
         <div className={styles.project}>
           <div className={styles.imageWrapper}>
-            <Image
-              src={"/images/portfolio/schuelertreff-erp-system.png"}
-              alt={"ERP SaaS System - Schuelertreff"}
-              width="800"
-              height="800"
-            />
+            <Link href={"/projects/saas-erp-system"}>
+              <Image
+                src={"/images/portfolio/schuelertreff-erp-system.png"}
+                alt={"ERP SaaS System - Schuelertreff"}
+                width="800"
+                height="800"
+              />
+            </Link>
             <div className={styles.textWrapper}>
               <h3>SaaS ERP System</h3>
               <p>
@@ -73,12 +77,14 @@ const Portfolio = () => {
         </div>
         <div className={styles.project}>
           <div className={styles.imageWrapper}>
-            <Image
-              src={"/images/portfolio/ar-in-surgery-hololens-2.png"}
-              alt={"AR in Surgery - Hololens 2"}
-              width="800"
-              height="800"
-            />
+            <Link href={"/projects/ar-in-surgery-hololens-2"}>
+              <Image
+                src={"/images/portfolio/ar-in-surgery-hololens-2.png"}
+                alt={"AR in Surgery - Hololens 2"}
+                width="800"
+                height="800"
+              />
+            </Link>
             <div className={styles.textWrapper}>
               <h3>AR in Surgery - Hololens&nbsp;2</h3>
               <p>
@@ -94,12 +100,14 @@ const Portfolio = () => {
         </div>
         <div className={styles.project}>
           <div className={styles.imageWrapper}>
-            <Image
-              src={"/images/portfolio/m-to-b-agency-homepage.png"}
-              alt={"Software Agency Homepage M-to-B"}
-              width="800"
-              height="800"
-            />
+            <Link href={"/projects/software-agency-homepage"}>
+              <Image
+                src={"/images/portfolio/m-to-b-agency-homepage.png"}
+                alt={"Software Agency Homepage M-to-B"}
+                width="800"
+                height="800"
+              />
+            </Link>
             <div className={styles.textWrapper}>
               <h3>Software Agency Homepage</h3>
               <p>
@@ -123,12 +131,14 @@ const Portfolio = () => {
         </div>
         <div className={styles.project}>
           <div className={styles.imageWrapper}>
-            <Image
-              src={"/images/portfolio/ar-in-surgery-ios-visionos.png"}
-              alt={"AR in Surgery - Hololens 2"}
-              width="800"
-              height="800"
-            />
+            <Link href={"/projects/ar-in-surgery-ios-visionos"}>
+              <Image
+                src={"/images/portfolio/ar-in-surgery-ios-visionos.png"}
+                alt={"AR in Surgery - Hololens 2"}
+                width="800"
+                height="800"
+              />
+            </Link>
             <div className={styles.textWrapper}>
               <h3>AR in Surgery - iOS & Apple&nbsp;Vision&nbsp;Pro</h3>
               <p>
@@ -144,12 +154,16 @@ const Portfolio = () => {
         </div>
         <div className={styles.project}>
           <div className={styles.imageWrapper}>
-            <Image
-              src={"/images/portfolio/special-solutions-management-system.png"}
-              alt={"Management SaaS System - Special Solutions"}
-              width="800"
-              height="800"
-            />
+            <Link href={"/projects/saas-management-system"}>
+              <Image
+                src={
+                  "/images/portfolio/special-solutions-management-system.png"
+                }
+                alt={"Management SaaS System - Special Solutions"}
+                width="800"
+                height="800"
+              />
+            </Link>
             <div className={styles.textWrapper}>
               <h3>SaaS Management System</h3>
               <p>
